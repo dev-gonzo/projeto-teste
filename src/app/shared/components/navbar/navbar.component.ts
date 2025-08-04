c86864224aec9bb1cb7807dd894437080c3da72b
Gustavo Lima Costa <glcosta@almavivasolutions.com.br>
2025-08-04 15:00:14 -0300

    feat: realizando alguns testes

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+    encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent {
   usuario = "Nome do Usuário";

f63bf59b255be3c69fd3ce35ccf2cfcab92ba4af
gustavo_lima <glcosta@almavivasolutions.com.br>
2025-08-12 10:28:57 -0300

    feat: adicionando as paginas de resposta

---
+++ src/app/shared/components/erro/erro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-erro',
+  standalone: true,
+  templateUrl: './erro.component.html',
+  styleUrl: './erro.component.scss'
+})
+export class ErroComponent {
+
+}

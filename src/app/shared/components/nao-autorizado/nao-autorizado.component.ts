359efae5b325d6dd5a7a9ff90b01fe366b70f292
gustavo_lima <glcosta@almavivasolutions.com.br>
2025-08-12 10:28:57 -0300

    feat: adicionando as paginas de resposta

---
+++ src/app/shared/components/nao-autorizado/nao-autorizado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nao-autorizado',
+  standalone: true,
+  templateUrl: './nao-autorizado.component.html',
+  styleUrl: './nao-autorizado.component.scss'
+})
+export class NaoAutorizadoComponent {
+
+}
---
+++ src/app/shared/components/nao-encontrada/nao-encontrada.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nao-encontrada',
+  standalone: true,
+  templateUrl: './nao-encontrada.component.html',
+  styleUrl: './nao-encontrada.component.scss'
+})
+export class NaoEncontradaComponent {
+
+}

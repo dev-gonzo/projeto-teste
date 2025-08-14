99f526d04bb3de6a6a305afaa1c8e29b498631a7
gustavo_lima <glcosta@almavivasolutions.com.br>
2025-08-14 17:03:35 -0300

    feat: implementar UnidadeOperacionalService e testes

--- src/app/core/services/unidade-operacional.service.ts
+++ src/app/core/services/unidade-operacional.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable, filter, take, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { CookieService } from 'ngx-cookie-service';
+import { keys } from '../../shared/utils/variables';
 import {
   UnidadeOperacional,
   HistoricoAcoes,
@@ -12,101 +13,121 @@ import {
   ResponseSuccessHttp,
   PageResponse,
   ListaUnidadeOperacional,
+  UnidadeOperacionalPageResponse,
+  Municipio,
+  Endereco,
 } from '../../shared/models';
 import { environment } from '../../../environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UnidadeOperacionalService {
-  private readonly endpoint = `${environment.apiUrl}/unidades`;
+  private readonly endpoint = `${environment.apiUrl}/unidade-operacional`;
   public readonly urlUpload = `${environment.apiUrl}/unidades/upload-file`;
   private readonly urlLogs = `${environment.apiUrl}/unidades/logs`;
-  private readonly endpointAutocadastro = `${environment.apiUrl}/unidade-operacional`;
 
-  private readonly jsonHeaders = new HttpHeaders({
-    'Content-Type': 'application/json; charset=utf-8',
-  });
+  private readonly jsonHeaders = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
 
-  constructor(private readonly http: HttpClient) { }
+  constructor(
+    private readonly http: HttpClient,
+    private readonly cookieService: CookieService,
+    private readonly authService: AuthService
+  ) { }
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return token ? this.jsonHeaders.set('Authorization', `Bearer ${token}`) : this.jsonHeaders;
+  }
 
   query(params: HttpParams): Observable<Page<UnidadeOperacional>> {
-    return this.http.get<PageResponse<UnidadeOperacional>>(this.endpoint, { params }).pipe(
-      map(response => PageImpl.of(response.content, response.totalElements))
+    const token = this.authService.getToken();
+    const headers = token
+      ? this.jsonHeaders.set('Authorization', `Bearer ${token}`)
+      : this.jsonHeaders;
+
+    return this.http.get<any>(this.endpoint, { params, headers }).pipe(
+      map(response => {
+        const content: UnidadeOperacional[] = (response._embedded?.unidadeOperacionalDTOList || []).map(
+          (unidadeDTO: any) => ({
+            id: unidadeDTO.id,
+            nomeUnidadeOperacional: unidadeDTO.nomeUnidadeOperacional,
+            responsavelUnidadeOperacional: unidadeDTO.responsavelUnidadeOperacional,
+            status: unidadeDTO.status,
+            numeroTelefonePrincipal: unidadeDTO.numeroTelefonePrincipal,
+            numeroTelefoneSecundario: unidadeDTO.numeroTelefoneSecundario,
+            telefoneCompleto: [unidadeDTO.numeroTelefonePrincipal, unidadeDTO.numeroTelefoneSecundario]
+              .filter(Boolean)
+              .join(' / '),
+
+            endereco: {
+              municipioNome: unidadeDTO.endereco?.municipioNome,
+              bairro: unidadeDTO.endereco?.bairro,
+              logradouro: unidadeDTO.endereco?.logradouro,
+              numero: unidadeDTO.endereco?.numero,
+              cep: unidadeDTO.endereco?.cep,
+              estadoSigla: unidadeDTO.endereco?.estadoSigla,
+            } as Endereco
+          })
+        );
+
+        const totalElements = response.page?.totalElements || content.length;
+
+        return PageImpl.of(content, totalElements);
+      })
     );
   }
 
+
   getLogs(params: HttpParams, id: number): Observable<Page<HistoricoAcoes>> {
-    return this.http.get<PageResponse<HistoricoAcoes>>(`${this.urlLogs}/${id}`, { params }).pipe(
-      map(response => PageImpl.of(response.content, response.totalElements))
-    );
+    return this.http
+      .get<PageResponse<HistoricoAcoes>>(`${this.urlLogs}/${id}`, { params, headers: this.getAuthHeaders() })
+      .pipe(map(response => PageImpl.of(response.content, response.totalElements)));
   }
 
   getAll(): Observable<UnidadeOperacional[]> {
-    return this.http.get<UnidadeOperacional[]>(`${this.endpoint}/findall`);
+    return this.http.get<UnidadeOperacional[]>(`${this.endpoint}/findall`, { headers: this.getAuthHeaders() });
   }
 
   listaTodasUnidadesOperacionais(): Observable<ListaUnidadeOperacional[]> {
-    return this.http.get<ListaUnidadeOperacional[]>(`${this.endpointAutocadastro}/list-all`);
+    return this.http.get<ListaUnidadeOperacional[]>(`${this.endpoint}/list-all`);
   }
 
   listByName(nome: string): Observable<UnidadeOperacional[]> {
-    return this.http.get<UnidadeOperacional[]>(`${this.endpoint}/nome/${nome}`);
+    return this.http.get<UnidadeOperacional[]>(`${this.endpoint}/nome/${nome}`, { headers: this.getAuthHeaders() });
   }
 
   insert(model: UnidadeOperacional): Observable<ResponseSuccessHttp> {
-    return this.http.post<ResponseSuccessHttp>(this.endpoint, model, {
-      headers: this.jsonHeaders,
-    });
+    return this.http.post<ResponseSuccessHttp>(this.endpoint, model, { headers: this.getAuthHeaders() });
   }
 
   update(model: UnidadeOperacional): Observable<ResponseSuccessHttp> {
-    return this.http.put<ResponseSuccessHttp>(`${this.endpoint}/${model.id}`, model, {
-      headers: this.jsonHeaders,
-    });
+    return this.http.put<ResponseSuccessHttp>(`${this.endpoint}/${model.id}`, model, { headers: this.getAuthHeaders() });
   }
 
   delete(id: number | undefined): Observable<ResponseSuccessHttp> {
-    if (!id) {
-      return throwError(() => new Error('ID is required'));
-    }
-
-    return this.http.delete<ResponseSuccessHttp>(`${this.endpoint}/${id}`, {
-      headers: this.jsonHeaders,
-    });
+    if (!id) return throwError(() => new Error('ID is required'));
+    return this.http.delete<ResponseSuccessHttp>(`${this.endpoint}/${id}`, { headers: this.getAuthHeaders() });
   }
 
   findById(id: number): Observable<UnidadeOperacional> {
-    return this.http.get<UnidadeOperacional>(`${this.endpoint}/${id}`);
+    return this.http.get<UnidadeOperacional>(`${this.endpoint}/${id}`, { headers: this.getAuthHeaders() });
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<UnidadeOperacional> {
     const id = route.paramMap.get('id');
-    if (!id) {
-      return throwError(() => new Error('ID não encontrado na rota'));
-    }
-
-    return this.findById(Number(id)).pipe(
-      filter(model => !!model),
-      take(1)
-    );
+    if (!id) return throwError(() => new Error('ID não encontrado na rota'));
+    return this.findById(Number(id)).pipe(filter(model => !!model), take(1));
   }
 
   baixarModelo(): Observable<Blob> {
     return this.http
-      .get(`${this.endpoint}/arquivo-UnidadeOperacional`, { responseType: 'blob' })
-      .pipe(
-        map(data => new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }))
-      );
+      .get(`${this.endpoint}/arquivo-UnidadeOperacional`, { responseType: 'blob', headers: this.getAuthHeaders() })
+      .pipe(map(data => new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })));
   }
 
   finalizarUpload(): Observable<ResponseSuccessHttp> {
-    return this.http.post<ResponseSuccessHttp>(
-      `${this.endpoint}/finalizar`,
-      {},
-      {
-        headers: this.jsonHeaders,
-      }
-    );
+    return this.http.post<ResponseSuccessHttp>(`${this.endpoint}/finalizar`, {}, { headers: this.getAuthHeaders() });
   }
 }
